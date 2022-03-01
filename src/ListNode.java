2f287a1351868ec1bc0bc0762e52739ece3bb081
yr0418 <1933352594@qq.com>
2022-03-02 00:07:54 +0800

    Initial commit

---
+++ src/ListNode.java
@@ -0,0 +1,15 @@
+/**
+ * @moduleName: ListNode
+ * @description: 链表节点
+ * @author: 杨睿
+ * @date: 2022-02-12 20:08
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
