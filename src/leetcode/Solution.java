bd7f60b3a0ecc97aac8f52ce510a153865bacfaa
yr0418 <1933352594@qq.com>
2024-04-03 00:13:29 +0800

    笔记更新

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -2,30 +2,54 @@ package leetcode;
 
 import utils.TreeNode;
 
+import java.util.*;
+import java.util.concurrent.DelayQueue;
 import java.util.logging.Logger;
 
 class Solution {
     private static final Logger log = Logger.getLogger(Solution.class.getName());
 
+    private static Deque<Character> stack = new LinkedList<>();
 
-    public int countNodes(TreeNode root) {
-        if (root == null) {
-            return 0;
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> numCountMap = new HashMap<>();
+        Map<Integer, List<Integer>> numIndexMap = new HashMap<>();
+        int maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            if (numCountMap.containsKey(current)) {
+                int count = numCountMap.get(current) + 1;
+                numCountMap.put(current, count);
+                numIndexMap.get(current).add(1, i);
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            } else {
+                numCountMap.put(current, 1);
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                numIndexMap.put(current, list);
+                if (maxCount < 1) {
+                    maxCount = 1;
+                }
+            }
         }
-        return getCountNodes(root);
-    }
 
-    public int getCountNodes(TreeNode node) {
-        if (node == null) {
-            return 0;
+        int minLen = nums.length;
+        for (Map.Entry<Integer, Integer> entry : numCountMap.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                List<Integer> list = numIndexMap.get(entry.getKey());
+                int len = list.size() <= 1 ? 1 : list.get(1) - list.get(0) + 1;
+                minLen = Math.min(minLen, len);
+            }
         }
-        return 1+ getCountNodes(node.left) + getCountNodes(node.right);
+        return minLen;
     }
-
-
-
 
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 2, 3, 1, 4, 2};
+        int len = solution.findShortestSubArray(nums);
+        log.info(String.valueOf(len));
     }
 }
