9b4b1d7296da02806470f5397a0acd622e9888a4
yr0418 <1933352594@qq.com>
2024-04-12 13:06:42 +0800

    笔记更新

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1,55 +1,32 @@
 package leetcode;
 
-import utils.TreeNode;
-
-import java.util.*;
-import java.util.concurrent.DelayQueue;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 class Solution {
     private static final Logger log = Logger.getLogger(Solution.class.getName());
 
-    private static Deque<Character> stack = new LinkedList<>();
-
-    public int findShortestSubArray(int[] nums) {
-        Map<Integer, Integer> numCountMap = new HashMap<>();
-        Map<Integer, List<Integer>> numIndexMap = new HashMap<>();
-        int maxCount = 0;
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numIndex = new HashMap<>();
+        int difference;
+        int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            int current = nums[i];
-            if (numCountMap.containsKey(current)) {
-                int count = numCountMap.get(current) + 1;
-                numCountMap.put(current, count);
-                numIndexMap.get(current).add(1, i);
-                if (count > maxCount) {
-                    maxCount = count;
-                }
+            int num = nums[i];
+            difference = target - num;
+            if (numIndex.containsKey(difference)) {
+                result[0] = i;
+                result[1] = numIndex.get(difference);
+                break;
             } else {
-                numCountMap.put(current, 1);
-                List<Integer> list = new ArrayList<>();
-                list.add(i);
-                numIndexMap.put(current, list);
-                if (maxCount < 1) {
-                    maxCount = 1;
-                }
+                numIndex.put(num, i);
             }
         }
-
-        int minLen = nums.length;
-        for (Map.Entry<Integer, Integer> entry : numCountMap.entrySet()) {
-            if (entry.getValue() == maxCount) {
-                List<Integer> list = numIndexMap.get(entry.getKey());
-                int len = list.size() <= 1 ? 1 : list.get(1) - list.get(0) + 1;
-                minLen = Math.min(minLen, len);
-            }
-        }
-        return minLen;
+        return result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {1, 2, 2, 3, 1, 4, 2};
-        int len = solution.findShortestSubArray(nums);
-        log.info(String.valueOf(len));
     }
 }
