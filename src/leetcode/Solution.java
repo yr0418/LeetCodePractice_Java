c75070e0a3b2c004d69037a66234b48ce44d5053
yr0418 <1933352594@qq.com>
2024-09-23 13:10:29 +0800

    笔记更新

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1,40 +1,56 @@
 package leetcode;
 
 import utils.ListNode;
-import utils.TreeNode;
 import utils.Utils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 class Solution {
     private static final Logger log = Logger.getLogger(Solution.class.getName());
 
-    public String convertToTitle(int columnNumber) {
-        StringBuilder sb = new StringBuilder();
-        while (columnNumber > 0) {
-            int mod = columnNumber % 26;
-            if (mod == 0) {
-                sb.append('Z');
-                columnNumber = columnNumber / 26 - 1;
+    public boolean validPalindrome(String s) {
+        boolean delete = false;
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+
+            if (s.charAt(i) == s.charAt(j)) {
+                i += 1;
+                j -= 1;
             } else {
-                sb.append((char)('A' + mod - 1));
-                columnNumber = columnNumber / 26;
+                if (delete) {
+                    return false;
+                } else {
+                    // 条件 1
+                    if (s.charAt(j - 1) == s.charAt(i)) {
+                        j -= 2;
+                        i += 1;
+                        delete = true;
+                    }
+                    // 条件 2
+                    else if (s.charAt(i + 1) == s.charAt(j)) {
+                        i += 2;
+                        j -= 1;
+                        delete = true;
+                    } else {
+                        return false;
+                    }
+                }
             }
         }
-        return sb.reverse().toString();
+        return true;
     }
-
-
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] strs = {"dog", "racecar", "car"};
+        String[] strs = {"Hello", "Alaska", "Dad", "Peace"};
         String str = "{[]}";
-        int[] intArr = {7,20,5,3,6,4};
-        Object result = null;
+        int[] intArr = {-1};
         ListNode list = Utils.buildLinkList(intArr);
+
+        Object result = solution.validPalindrome("ebcbbececabbacecbbcbe");
         log.info(String.valueOf(result));
     }
 }
